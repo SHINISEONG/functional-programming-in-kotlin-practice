2951336d4a7102e4dd8f82faa14f60138033f0c8
shineseong <hss275989@gmail.com>
2023-11-27 18:11:22 +0900

    챕터3 예문 3, 4, 1, 11, 12 코딩

---
+++ src/main/kotlin/chapter3/examples/list12/list.kt
@@ -0,0 +1,11 @@
+package chapter3.examples.list12
+
+sealed class Tree<out A>
+data class Leaf<A>(val value: A) : Tree<A>()
+data class Branch<A>(val left: Tree<A>, val right: Tree<A>) : Tree<A>()
+
+fun <A> size(tree: Tree<A>): Int =
+    when (tree) {
+        is Leaf -> 1
+        is Branch -> 1 + size(tree.left) + size(tree.right)
+    }
